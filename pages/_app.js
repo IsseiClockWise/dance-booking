6768f70bfdae4a6bbfe0940f237b712106f8f74b
IsseiClockWise <112615226+IsseiClockWise@users.noreply.github.com>
2023-08-05 15:14:35 +0900

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
 import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Navbar, Container } from 'react-bootstrap'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+            ダンス予約システム
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
